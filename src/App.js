a725857e099e6287e6f9b02392ce27fec282f791
Your Name <you@example.com>
2024-06-26 10:14:02 +0530

    app.js updaed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        App.js
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const App = () => {
+ const [todos, setTodos] = useState([]);
+ const [text, setText] = useState('');
+ useEffect(() => {
+ axios.get('http://localhost:5000/todos')
+ .then(response => setTodos(response.data))
+ .catch(error => console.error('Error fetching data: ', error));
+ }, []);
+ const addTodo = () => {
+ if (text) {
+ axios.post('http://localhost:5000/todos', { text })
+ .then(response => setTodos([...todos, response.data]))
+ .catch(error => console.error('Error adding todo: ', error));
+ setText('');
+ }
+ };
+ const deleteTodo = (id) => {
+ axios.delete(`http://localhost:5000/todos/${id}`)
+ .then(response => setTodos(todos.filter(todo => todo._id !== id)))
+ .catch(error => console.error('Error deleting todo: ', error));
+ };
+ return (
+ <div className="App">
+ <h1>To-Do List</h1>
+ <input
+ type="text"
+ value={text}
+ onChange={(e) => setText(e.target.value)} 
+ placeholder="Add a new task" 
+ />
+ <button onClick={addTodo}>Add</button>
+ <ul>
+ {todos.map(todo => (
+ <li key={todo._id}>
+ {todo.text}
+ <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+ </li>
+ ))}
+ </ul>
+ </div>
+ );
+};
+export default App
