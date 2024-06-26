d5c2f4682caca9746de505a3502ca89da35d4288
Your Name <you@example.com>
2024-06-26 09:54:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        App.js
+      </header>
+    </div>
+  );
+}
+
+export default App;
